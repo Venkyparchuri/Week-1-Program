4cb7d76651d848863b616acc85189924e642e801
Venkyparchuri <80995095+Venkyparchuri@users.noreply.github.com>
2021-09-03 16:45:47 -0700

    Create Design pattern.java

---
+++ Design pattern.java
@@ -0,0 +1,56 @@
+package com.vogella.java.designpattern.observer;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyModel {
+public static final String FIRSTNAME = "firstName";
+public static final String LASTNAME = "lastName";
+
+private List<Person> persons = new ArrayList<Person>();
+private List<PropertyChangeListener> listener = new ArrayList<PropertyChangeListener>();
+
+public class Person {
+
+private String firstName;
+
+private String lastName;
+
+public Person(String firstName, String lastName) {
+this.firstName = firstName;
+this.lastName = lastName;
+}
+
+public String getFirstName() {
+
+return firstName;
+}
+
+public void setFirstName(String firstName) {
+notifyListeners(
+this,
+FIRSTNAME,
+this.firstName,
+this.firstName = firstName);
+
+}
+
+public String getLastName() {
+return lastName;
+}
+
+public void setLastName(String lastName) {
+notifyListeners(
+this,
+LASTNAME,
+this.lastName,
+this.lastName = lastName);
+}
+}
+
+public List<Person> getPersons() {
+return persons;
+}
+
